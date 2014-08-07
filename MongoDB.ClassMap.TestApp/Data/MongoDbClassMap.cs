b433c715005a0961cf3179c23e47bec419efdc63
Alex Brown <alex@alexjamesbrown.com>
2014-08-07 23:40:29 +0100

    Added proof of concept example

---
+++ MongoDB.ClassMap.TestApp/Data/MongoDbClassMap.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization;
+
+namespace MongoDB.ClassMap.TestApp.Data
+{
+    public abstract class MongoDbClassMap<T>
+    {
+        protected MongoDbClassMap()
+        {
+            if (!BsonClassMap.IsClassMapRegistered(typeof(T)))
+                BsonClassMap.RegisterClassMap<T>(Map);
+        }
+
+        public abstract void Map(BsonClassMap<T> cm);
+    }
+}
